bc392c3c417ef3029afff5c1a5ea4dac471b1053
hrpr <ryan@hrpr.dev>
2025-02-17 21:17:56 +0000

    set nerd-fonts

--- modules/system.nix
+++ modules/system.nix
@@ -178,11 +178,7 @@
       # icon fonts
       material-design-icons
       font-awesome
-
-      # nerdfonts
-      # https://github.com/NixOS/nixpkgs/blob/nixos-24.05/pkgs/data/fonts/nerdfonts/shas.nix
-      (nerdfonts.override {
-        fonts = [
+      nerd-fonts = [
           # symbols icon only
           "NerdFontsSymbolsOnly"
           # Characters
