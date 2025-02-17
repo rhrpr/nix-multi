f16d3a189fcd47af6d50318114077276f2c51668
hrpr <ryan@hrpr.dev>
2025-02-17 21:14:26 +0000

    remove brew mirror

--- flake.nix
+++ flake.nix
@@ -66,7 +66,6 @@
         ./modules/nix-core.nix
         ./modules/system.nix
         ./modules/apps.nix
-        ./modules/homebrew-mirror.nix # comment this line if you don't need a homebrew mirror
         ./modules/host-users.nix
 
         # home manager
