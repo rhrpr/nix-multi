861c5a9e536316c95dbfe33ae16f396eb1ddd910
hrpr <ryan@hrpr.dev>
2025-03-01 09:44:07 +0000

    cloud prompt indicators

--- home/starship.nix
+++ home/starship.nix
@@ -15,14 +15,22 @@
           success_symbol = "[›](bold green)";
           error_symbol = "[›](bold red)";
         };
+        time = {
+          disabled = false;
+          format = "at [$time]($style)";
+          time_format = "%T";
+        };
         aws = {
           symbol = "🅰 ";
         };
+        azure = {
+          symbol = "󰠅"
+        }
         gcloud = {
           # do not show the account/project's info
           # to avoid the leak of sensitive information when sharing the terminal
           format = "on [$symbol$active(\($region\))]($style) ";
-          symbol = "🅶 ️";
+          symbol = "🅶 ";
         };
       };
   };
