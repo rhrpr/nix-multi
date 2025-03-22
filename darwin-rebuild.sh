530542ccba20710f32655dc1ac4a59aca3bfc332
hrpr <ryan@hrpr.dev>
2025-03-22 12:02:44 +0000

    improve rebuild script

--- darwin-rebuild.sh
+++ darwin-rebuild.sh
@@ -1,3 +1,3 @@
-nix flake update /etc/nix-darwin
-nix flake update
+nix flake update --flake /etc/nix-darwin
+nix flake update --flake ~/.config/darwin/
 darwin-rebuild switch --flake ~/.config/darwin/
