8ca9f6991dec25c8b23e6f5c4dc69e05f67e840b
hrpr <ryan@hrpr.dev>
2025-03-22 12:11:23 +0000

    nvim plugins

--- home/nvim.nix
+++ home/nvim.nix
@@ -3,5 +3,18 @@
   programs.neovim = {
     enable = true;
     package = inputs.nvchad4nix.packages.${inputs.system}.default;
+    extraPackages = with pkgs; [
+      nodePackages.bash-language-server
+      docker-compose-language-service
+      dockerfile-language-server-nodejs
+      emmet-language-server
+      nixd
+      (python3.withPackages(ps: with ps; [
+        python-lsp-server
+        flake8
+      ]))
+    ];
+    hm-activation = true;
+    backup = true;
   };
 }
