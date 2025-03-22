15bb80b01a1ae8fc3680663645c4943fcedfeb16
hrpr <ryan@hrpr.dev>
2025-03-22 11:00:19 +0000

    starship dracula theme, remove eza

--- home/core.nix
+++ home/core.nix
@@ -35,16 +35,6 @@
       defaultEditor = true;
       vimAlias = true;
     };
-
-    # A modern replacement for ‘ls’
-    # useful in bash/zsh prompt, not in nushell.
-    eza = {
-      enable = true;
-      git = true;
-      icons = "auto";
-      enableZshIntegration = true;
-    };
-
     # terminal file manager
     yazi = {
       enable = true;
--- home/shell.nix
+++ home/shell.nix
@@ -11,7 +11,7 @@
   
   home.shellAliases = {
     k = "kubectl";
-    l = "eza -alg --sort oldest --git -group-folders-first";
-    latr = "ls -latr";
+    l = "ls -al";
+    latr = "ls -altr";
   };
 }
