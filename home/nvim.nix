91d729ec47460dcfb39e4d83a5d39066da0a4333
hrpr <ryan@hrpr.dev>
2025-04-06 08:16:32 +0100

    nvim settings comments

--- home/nvim.nix
+++ home/nvim.nix
@@ -5,9 +5,9 @@
     package = inputs.nvchad4nix.packages.${inputs.system}.default;
     # Add custom Neovim configuration
     extraConfig = ''
-      set expandtab
-      set tabstop=2
-      set shiftwidth=2
+      set expandtab # Use spaces instead of tabs
+      set tabstop=2 # Number of spaces to use for each tab
+      set shiftwidth=2 # Number of spaces to use for autoindent
     '';
     extraPackages = with pkgs; [
       nodePackages.bash-language-server
