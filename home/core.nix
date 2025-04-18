dbf09b824dcc9f17214ab8f81bebb057d3cec9c6
hrpr <ryan@hrpr.dev>
2025-04-18 14:07:18 +0100

    Revert "undo removal of core.nix content"
    This reverts commit 77671cf9f17236838c64b605ca676bee0286037e.

--- home/core.nix
+++ home/core.nix
@@ -1,57 +1,23 @@
-{pkgs, ...}: {
-  home.packages = with pkgs; [
-    # archives
-    zip
-    xz
-    unzip
-    p7zip
+{ pkgs, ... }:
 
-    # utils
-    ripgrep # recursively searches directories for a regex pattern
-    jq # A lightweight and flexible command-line JSON processor
-    yq-go # yaml processer https://github.com/mikefarah/yq
-    fzf # A command-line fuzzy finder
-    dnsutils
-    aria2 # A lightweight multi-protocol & multi-source command-line download utility
-    socat # replacement of openbsd-netcat
-    nmap # A utility for network discovery and security auditing
+{
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    initExtra = ''
+      gr() {
+        rg -rnIi --colour "$1" ./
+      }
+    '';
+  };
 
-    # misc
-    file
-    which
-    tree
-    gawk
-    zstd
-    gnupg
-
-    # productivity
-    glow # markdown previewer in terminal
-  ];
-
-  programs = {
-    # modern vim
-    neovim = {
-      enable = true;
-      defaultEditor = true;
-      vimAlias = true;
-    };
-    # terminal file manager
-    yazi = {
-      enable = true;
-      enableZshIntegration = true;
-      settings = {
-        manager = {
-          show_hidden = true;
-          sort_dir_first = true;
-        };
-      };
-    };
-
-    # skim provides a single executable: sk.
-    # Basically anywhere you would want to use grep, try sk instead.
-    skim = {
-      enable = true;
-      enableBashIntegration = true;
-    };
+  home.shellAliases = {
+    k = "kubectl";
+    l = "ls -altr";
+    ll = "ls -altr";
+    ls = "ls --color=auto";
+    latr = "ls -altr";
+    darwinup = "/Users/hrpr/.config/darwin/darwin-rebuild.sh";
+    rebuild = "/Users/hrpr/.config/darwin/darwin-rebuild.sh";
   };
 }
