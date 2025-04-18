035bdec23143a389f8a836ae4cb15efa05c360e2
hrpr <ryan@hrpr.dev>
2025-04-18 14:24:49 +0100

    fix mas repeat installs, vscode changes in property names

--- home/vscode.nix
+++ home/vscode.nix
@@ -4,52 +4,54 @@
   programs.vscode = {
     enable = true;
     package = pkgs.vscode;
-    
-    # Extensions
-    # To get your current extensions, run: code --list-extensions
-    extensions = with pkgs.vscode-extensions; [
-      # Common extensions - uncomment or add your own
-      bbenoist.nix
-      davidanson.vscode-markdownlint
-      github.copilot
-      github.copilot-chat
-      ms-python.debugpy
-      ms-python.python
-      ms-python.vscode-pylance
-      ms-vscode-remote.remote-ssh
-      ms-vscode-remote.remote-ssh-edit
-      mechatroner.rainbow-csv
-    ] 
-    ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-      # For extensions not in nixpkgs:
-      # {
-      #   name = "extension-name";
-      #   publisher = "publisher";
-      #   version = "version";
-      #   sha256 = "sha256-hash"; # Generate with nix-prefetch-url
-      # }
-    ];
-    
-    # Your settings
-    # Copy from: ~/Library/Application Support/Code/User/settings.json
-    userSettings = {
-      "editor.fontSize" = 14;
-      "editor.fontFamily" = "Menlo, Monaco, 'Courier New', monospace";
-      "editor.tabSize" = 2;
-      "workbench.colorTheme" = "Kimbie Dark";
-      "files.autoSave" = "afterDelay";
-      # Add your other settings here
+
+    profiles.default = {
+      # Extensions
+      # To get your current extensions, run: code --list-extensions
+      extensions = with pkgs.vscode-extensions; [
+        # Common extensions - uncomment or add your own
+        bbenoist.nix
+        davidanson.vscode-markdownlint
+        github.copilot
+        github.copilot-chat
+        ms-python.debugpy
+        ms-python.python
+        ms-python.vscode-pylance
+        ms-vscode-remote.remote-ssh
+        ms-vscode-remote.remote-ssh-edit
+        mechatroner.rainbow-csv
+      ] 
+      ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+        # For extensions not in nixpkgs:
+        # {
+        #   name = "extension-name";
+        #   publisher = "publisher";
+        #   version = "version";
+        #   sha256 = "sha256-hash"; # Generate with nix-prefetch-url
+        # }
+      ];
+
+      # Your settings
+      # Copy from: ~/Library/Application Support/Code/User/settings.json
+      userSettings = {
+        "editor.fontSize" = 14;
+        "editor.fontFamily" = "Menlo, Monaco, 'Courier New', monospace";
+        "editor.tabSize" = 2;
+        "workbench.colorTheme" = "Kimbie Dark";
+        "files.autoSave" = "afterDelay";
+        # Add your other settings here
+      };
+
+      # Your keybindings
+      # Copy from: ~/Library/Application Support/Code/User/keybindings.json
+      keybindings = [
+        # Example:
+        # {
+        #   key = "cmd+k cmd+i";
+        #   command = "editor.action.showHover";
+        #   when = "editorTextFocus";
+        # }
+      ];
     };
-    
-    # Your keybindings
-    # Copy from: ~/Library/Application Support/Code/User/keybindings.json
-    keybindings = [
-      # Example:
-      # {
-      #   key = "cmd+k cmd+i";
-      #   command = "editor.action.showHover";
-      #   when = "editorTextFocus";
-      # }
-    ];
   };
 }
--- modules/apps.nix
+++ modules/apps.nix
@@ -72,6 +72,7 @@
       "jansson"        # C library for encoding/decoding JSON
       "libidn2"        # International domain name library
       "lume"           # Command-line tool for virtual machines
+      "mas"            # Mac App Store command line interface
       "nettle"         # Low-level cryptographic library
       "openssl@3"      # Cryptography and SSL/TLS toolkit
       "p11-kit"        # Library to load and share PKCS#11 modules
