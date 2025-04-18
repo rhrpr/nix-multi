dd64f954aea5d2762bb83c284f73190afd47eeb1
hrpr <ryan@hrpr.dev>
2025-04-18 14:17:13 +0100

    update home-manager to unstable (master)

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
+      url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
