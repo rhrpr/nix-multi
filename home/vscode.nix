3979227f2b7ab9c8c29f6164f03944dda1943d29
hrpr <ryan@hrpr.dev>
2025-04-21 11:12:00 +0100

    fix vscode issues (drop editor config support)

--- home/vscode.nix
+++ home/vscode.nix
@@ -6,10 +6,7 @@
     package = pkgs.vscode;
 
     profiles.default = {
-      # Extensions
-      # To get your current extensions, run: code --list-extensions
       extensions = with pkgs.vscode-extensions; [
-        # Common extensions - uncomment or add your own
         bbenoist.nix
         davidanson.vscode-markdownlint
         github.copilot
@@ -20,34 +17,31 @@
         ms-vscode-remote.remote-ssh
         ms-vscode-remote.remote-ssh-edit
         mechatroner.rainbow-csv
-        # Added TypeScript support
         dbaeumer.vscode-eslint
-        # Added GitHub Actions
         github.vscode-github-actions
-        swiftlang.swift-vscode
-      ]
-
-      # Your settings
-      # Copy from: ~/Library/Application Support/Code/User/settings.json
-      userSettings = {
-        "editor.fontSize" = 14;
-        "editor.fontFamily" = "Menlo, Monaco, 'Courier New', monospace";
-        "editor.tabSize" = 2;
-        "workbench.colorTheme" = "Kimbie Dark";
-        "files.autoSave" = "afterDelay";
-        # Add your other settings here
-      };
-
-      # Your keybindings
-      # Copy from: ~/Library/Application Support/Code/User/keybindings.json
-      keybindings = [
-        # Example:
-        # {
-        #   key = "cmd+k cmd+i";
-        #   command = "editor.action.showHover";
-        #   when = "editorTextFocus";
-        # }
       ];
     };
   };
 }
+
+      # Somehow busted 21.04.2025 # Your settings
+      # # Copy from: ~/Library/Application Support/Code/User/settings.json
+      # userSettings = {
+      #   "editor.fontSize" = 14;
+      #   "editor.fontFamily" = "Menlo, Monaco, 'Courier New', monospace";
+      #   "editor.tabSize" = 2;
+      #   "workbench.colorTheme" = "Kimbie Dark";
+      #   "files.autoSave" = "afterDelay";
+      #   # Add your other settings here
+      # };
+
+      # # Your keybindings
+      # # Copy from: ~/Library/Application Support/Code/User/keybindings.json
+      # keybindings = [
+      #   # Example:
+      #   # {
+      #   #   key = "cmd+k cmd+i";
+      #   #   command = "editor.action.showHover";
+      #   #   when = "editorTextFocus";
+      #   # }
+      # ];
