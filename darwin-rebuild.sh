1d72e2a3e041101975437e1b6d79138816b722e2
hrpr <ryan@hrpr.dev>
2025-04-26 21:17:17 +0100

    rebuild script, flake lock

--- darwin-rebuild.sh
+++ darwin-rebuild.sh
@@ -1,2 +1,2 @@
-nix flake update --flake ~/.config/darwin/
-darwin-rebuild switch --flake ~/.config/darwin/ --show-trace
+nix flake update --flake ~/.config/nix-darwin/
+darwin-rebuild switch --flake ~/.config/nix-darwin/ --show-trace
