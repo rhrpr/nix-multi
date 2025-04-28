8633eeb4ad9250df118ddbee8e45673995bdcaab
hrpr <ryan@hrpr.dev>
2025-04-28 20:33:33 +0100

    update push and build script

--- darwin-rebuild.sh
+++ darwin-rebuild.sh
@@ -5,7 +5,14 @@ else
   echo "No files added."
 fi
 
-read -p "Enter commit message: " commit_message
-git commit -m "$commit_message"
+read -p "Enter commit message (leave empty to skip): " commit_message
+if [ -n "$commit_message" ]; then
+  git commit -m "$commit_message"
+else
+  echo "No commit made."
+fi
 nix flake update --flake ~/.config/nix-darwin/
+git add flake.lock
+git commit -m "Update flake.lock"
+git push origin main
 darwin-rebuild switch --flake ~/.config/nix-darwin/ --show-trace
