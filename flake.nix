cdcb5383dffc3f9221cc5adf104a55037a98780e
hrpr <ryan@hrpr.dev>
2025-06-08 13:47:10 +0100

    switch to hyprland

--- flake.nix
+++ flake.nix
@@ -72,7 +72,7 @@
     baseSpecialArgs = inputs // {
       inherit username useremail;
       # Desktop manager selection (plasma/hyprland for Linux)
-      desktopManager = "plasma"; # Change this to "hyprland" for Hyprland
+      desktopManager = "hyprland"; # Change this to "hyprland" for Hyprland
     };
 
     # Common home-manager configuration function
