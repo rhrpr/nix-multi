7af1d970493e531d190e2096f96f76822e663522
hrpr <ryan@hrpr.dev>
2025-06-17 09:17:25 +0100

    remove unrequired apps

--- modules/darwin/apps.nix
+++ modules/darwin/apps.nix
@@ -113,7 +113,6 @@
       "mixxx"          # DJ software
       "moonlight"      # Game streaming client
       "obsidian"       # Markdown knowledge base and note-taking app
-      "oversight"      # Monitors and notifies when microphone or camera is activated
       # "oversight"      # Monitors and notifies when microphone or camera is activated
       "protonvpn"      # VPN client
       "spotify"        # Music streaming service
