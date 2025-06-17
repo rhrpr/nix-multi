92a50fc0b612ac22e7454a8b6b60c4ff3bc5644d
hrpr <ryan@hrpr.dev>
2025-06-17 15:58:05 +0100

    allow unfree packages globally

--- flake.nix
+++ flake.nix
@@ -85,6 +85,12 @@
     };
     
     # Common special arguments for all configurations
+
+    # Allow unfree packages globally
+    nixpkgsConfig = {
+      allowUnfree = true;
+    };
+    
     baseSpecialArgs = inputs // {
       inherit username useremail gpuConfig;
     };
